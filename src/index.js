fa8e4f26ad5fc02e93c9b80e27594c3bf127a25a
nitin-codr <nitinporwal99@gmail.com>
2019-12-28 16:55:04 +0530

    adding middleware to index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import registerServiceWorker from './registerServiceWorker';
 import counterReducer from './store/reducers/counter';
@@ -13,7 +13,18 @@ const rootReducer = combineReducers({
     res: resultReducer
 })
 
-const store = createStore(rootReducer);
+const logger = state => {
+    return next => {
+        return action => {
+            console.log("[Middleware] Dispaching", action);
+            const result = next(action);
+            console.log("[Middleware] next state", state.getState());
+            return result;
+        }
+    }
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
